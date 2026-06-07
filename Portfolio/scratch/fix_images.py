fb369e9b85034afa6b2b9d5206dbbb4baef7014d
tglong-creator <trangianglong494@gmail.com>
2026-06-07 22:30:49 +0700

    Delete Portfolio directory

--- Portfolio/scratch/fix_images.py
+++
@@ -1,9 +0,0 @@
-with open('d:/UET/Ki 2/Nhập môn CNS & AI/Portfolio/js/main.js', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('src="assets/images/', 'src="assets/')
-
-with open('d:/UET/Ki 2/Nhập môn CNS & AI/Portfolio/js/main.js', 'w', encoding='utf-8') as f:
-    f.write(content)
-
-print("Fixed image paths!")
